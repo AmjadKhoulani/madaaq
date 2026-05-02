b48a202bd7c856d2821e0ee30347902f0227548f
Amjad <amjad@example.com>
2026-05-02 12:49:13 +0300

    Fix: Renamed index.js to index.cjs to support CommonJS require

--- start_api.cjs
+++ start_api.cjs
@@ -2,13 +2,15 @@ const { Client } = require('ssh2');
 const conn = new Client();
 
 conn.on('ready', () => {
-  console.log('🔗 Connected for API Setup');
-  conn.exec('npm install -g pm2 && cd /home/madaaq/public_html/server && npm install && pm2 delete madaaq-api || true && pm2 start index.js --name madaaq-api', (err, stream) => {
+  console.log('🔗 Connected for Full Reset and API Start');
+  const setupCmd = 'cd /home/madaaq/public_html && git fetch --all && git reset --hard origin/master && cd server && npm install && pm2 delete madaaq-api || true && pm2 start index.js --name madaaq-api';
+  
+  conn.exec(setupCmd, (err, stream) => {
     if (err) throw err;
     stream.on('data', (data) => process.stdout.write(data));
     stream.stderr.on('data', (data) => process.stderr.write(data));
     stream.on('close', () => {
-      console.log('\n🚀 API is now running in background via PM2');
+      console.log('\n🚀 API restarted with fresh code!');
       conn.end();
     });
   });
